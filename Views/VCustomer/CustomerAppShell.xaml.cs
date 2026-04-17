176f13984c0cf3dbdb79878a0219cbb6c8e4501e
willylengkong <willylengkonggg@gmail.com>
2026-04-18 00:13:24 +0700

    Update CustomerAppShell

--- Views/VCustomer/CustomerAppShell.xaml.cs
+++ Views/VCustomer/CustomerAppShell.xaml.cs
@@ -2,14 +2,19 @@ namespace AstroBoy.Views.VCustomer;
 
 public partial class CustomerAppShell : Shell
 {
-	public CustomerAppShell()
-	{
-		InitializeComponent();
+    public CustomerAppShell()
+    {
+        InitializeComponent();
 
-		// Daftarkan route untuk StoreDetailPage
-		Routing.RegisterRoute(nameof(StoreDetailPage), typeof(StoreDetailPage));
+        // Daftarkan route untuk StoreDetailPage
+        Routing.RegisterRoute(nameof(StoreDetailPage), typeof(StoreDetailPage));
 
-		// Daftarkan route untuk CartPage
-		Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
-	}
+        // Daftarkan route untuk CartPage
+        Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
+    }
+
+    private async void OnCartClicked(object sender, EventArgs e)
+    {
+        await GoToAsync(nameof(CartPage));
+    }
 }
